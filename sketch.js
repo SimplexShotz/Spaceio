e7d878b96d21320d0c58ed20ceab1acc60b4911c
SimplexShotz <brancy280@gmail.com>
2019-02-06 21:27:28 -0500

    Add Spaceship

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 
 var database, ref;
 
+var ship;
+
+var inf = {};
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   var config = {
@@ -16,8 +20,60 @@ function setup() {
   ref = {
     game: database.ref("game")
   };
+
+  ref.game.once("value", function(data) {
+    var d = data.val();
+    inf = d;
+  });
+
+  var shipPallet = [
+    color(0, 0, 0, 0),
+    color(0, 0, 0),
+    color(255, 255, 255),
+    color(225, 50, 50),
+    color(50, 200, 255)
+  ],
+  // color(50, 200, 255)
+  shipMap = [
+    "00000001110000000",
+    "00000001410000000",
+    "00000001110000000",
+    "00000001210000000",
+    "00000011211000000",
+    "00000012221000000",
+    "00011112221111000",
+    "00013112221131000",
+    "00012122222121000",
+    "11112422322421111",
+    "13114223332241131",
+    "12112223232221121",
+    "12122222222222121",
+    "12222232223222221",
+    "12221332223312221",
+    "12211331213311221",
+    "12111111211111121",
+    "11100001110000111"
+  ];
+
+  var g = createGraphics(17 * 5, 18 * 5, P2D);
+  g.noStroke();
+  for (var i = 0; i < shipMap[0].length; i++) {
+    for (var j = 0; j < shipMap.length; j++) {
+      g.fill(shipPallet[shipMap[j][i]]);
+      g.rect(i * 5, j * 5, 5, 5);
+    }
+  }
+  ship = g;
+}
+
+function Ship(dataRef) {
+  this.dataRef = dataRef;
+  this.draw = function() {
+
+  }
 }
 
 function draw() {
   background(50);
+  image(ship, 20, 20, 17 * 5, 18 * 5);
 }
