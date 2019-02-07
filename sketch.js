7422a335051622fd8cdbf58f6d51d3fa102ec07f
SimplexShotz <brancy280@gmail.com>
2019-02-06 20:37:48 -0500

    Add firebase

--- sketch.js
+++ sketch.js
@@ -1,5 +1,21 @@
+
+var database, ref;
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  var config = {
+    apiKey: "AIzaSyBRi2IbqvHVNNqYdZZ4G7kWIfwLydehd8I",
+    authDomain: "spaceio.firebaseapp.com",
+    databaseURL: "https://spaceio.firebaseio.com",
+    projectId: "spaceio",
+    storageBucket: "spaceio.appspot.com",
+    messagingSenderId: "617144049210"
+  };
+  firebase.initializeApp(config);
+  database = firebase.database();
+  ref = {
+    game: database.ref("game")
+  };
 }
 
 function draw() {
